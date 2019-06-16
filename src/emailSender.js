d502cd3d45730fe2c818aba5e00fc920dc7dafc4
Jonathan Booz <jon@jonbooz.com>
2019-06-16 09:55:21 -0700

    Add support sending to named users
    This is not general multi-user support, but the `emailSender.js` handler
    now takes a `user` parameter that determines which user to run the
    handler on. It still defaults to my user account though.

--- src/emailSender.js
+++ src/emailSender.js
@@ -4,12 +4,21 @@ const Environment = require('./engine/environment.js');
 const Configurator = require('./engine/configurator.js');
 const Context = require('./models/Context.js');
 
+const DEFAULT_USER = "jonbooz";
+
 const emailSenderHandler = async function(event, ctx, callback) {
     const environment = new Environment();
     const configurator = new Configurator(environment);
     const engine = configurator.buildEngine();
 
-    const user = await environment.getDataStore().getUser('jonbooz');
+    let userName = null;
+    if (event.hasOwnProperty('user')) {
+        userName = event.user;
+    } else {
+        userName = DEFAULT_USER;
+    }
+
+    const user = await environment.getDataStore().getUser(userName);
     const context = new Context();
     context.setUser(user);
     context.setLogLevel(environment.getLogLevel());
