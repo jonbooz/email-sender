ce3c9094a5f482053e19357c9ca6f2156c04a9b3
Jonathan Booz <jon@jonbooz.com>
2020-02-08 21:06:39 -0800

    Unit tests for ActiveModuleFilter
    Also removes the old processor-output test data.

---
+++ spec/engine/processes/ActiveModuleFilter.spec.ts
@@ -0,0 +1,37 @@
+import * as sinon from 'sinon';
+import {expect} from 'chai';
+
+import {BoundModule} from "../../../src/models/BoundModule";
+import {Record} from "../../../src/models/Record";
+import {ActiveModuleFilter} from "../../../src/engine/processes/ActiveModuleFilter";
+import {User} from "../../../src/models/User";
+
+const MODULES = require('../../../../../test-data/modules.json');
+const USERS = require('../../../../../test-data/users.json');
+
+beforeEach(() => {
+    this.sandbox = sinon.createSandbox();
+});
+
+afterEach(() => {
+    this.sandbox.restore();
+});
+
+describe('engine/processes/ActiveModuleFilter', () => {
+    it('filters modules for a user\'s active modules', async () => {
+        const filter = new ActiveModuleFilter();
+
+        const modules = MODULES['test'];
+        const user = new User(USERS['test']);
+
+        const boundModules = modules.map((m) => new BoundModule(user, m));
+
+        const record = new Record('test', user, boundModules);
+        const result = await filter.send(record);
+
+        const activeModuleNames = user.activeModules.map((am) => am.name);
+        const containsAll = result.modules.map((bm) => bm.activeModule.name)
+            .every((name) => activeModuleNames.includes(name));
+        expect(containsAll).to.be.true;
+    });
+});
